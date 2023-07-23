3da2782f1819e4d7d881aa31e2a456131b100ff7
Reuben Miller <reuben.d.miller@gmail.com>
2023-07-23 17:01:46 +0200

    remove unused code

--- images/setup.sh
+++ images/setup.sh
@@ -11,7 +11,6 @@ start_enable tedge-mapper-c8y
 start_enable c8y-configuration-plugin
 start_enable c8y-firmware-plugin
 start_enable c8y-log-plugin
-# rc-service rsyslog start
 
 echo "Service 'All': Status"
 tedgectl is_available
