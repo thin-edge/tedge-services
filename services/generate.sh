59a4a63f9f12a02d3779bcf7e3fa4133d3e786c2
Reuben Miller <reuben.d.miller@gmail.com>
2023-07-23 16:58:25 +0200

    chore(refactor): use bash files for template inputs over csv

--- services/generate.sh
+++ services/generate.sh
@@ -6,59 +6,106 @@ pushd "$SCRIPT_DIR" || exit 1
 
 execute_template() {
     input_file="$1"
-    shift
-
-    NAME="$1"
-    COMMAND="$2"
-    COMMAND_ARGS="$3"
-    DESCRIPTION="$4"
 
     sed \
-        -e "s/\\\$NAME/$NAME/g" \
-        -e "s/\\\$COMMAND_ARGS/$COMMAND_ARGS/g" \
-        -e "s/\\\$COMMAND/$COMMAND/g" \
-        -e "s/\\\$DESCRIPTION/$DESCRIPTION/g" \
+        -e "s/\\\$NAME/${NAME:-}/g" \
+        -e "s/\\\$LOG_NAME/${NAME:-}/g" \
+        -e "s/\\\$COMMAND_ARGS/${COMMAND_ARGS:-}/g" \
+        -e "s/\\\$COMMAND_USER/${COMMAND_USER:-}/g" \
+        -e "s/\\\$COMMAND/${COMMAND:-}/g" \
+        -e "s/\\\$DESCRIPTION/${DESCRIPTION:-}/g" \
+        "$input_file"
+}
+
+execute_template_inplace() {
+    input_file="$1"
+
+    sed -i \
+        -e "s/\\\$NAME/${NAME:-}/g" \
+        -e "s/\\\$LOG_NAME/${NAME:-}/g" \
+        -e "s/\\\$COMMAND_ARGS/${COMMAND_ARGS:-}/g" \
+        -e "s/\\\$COMMAND_USER/${COMMAND_USER:-}/g" \
+        -e "s/\\\$COMMAND/${COMMAND:-}/g" \
+        -e "s/\\\$DESCRIPTION/${DESCRIPTION:-}/g" \
         "$input_file"
 }
 
 generate_openrc() {
-    NAME="$1"
-
     mkdir -p openrc/conf.d
     mkdir -p openrc/init.d
     echo "[openrc]   Generating service file: $NAME"
-    execute_template "openrc/conf.d.template" "$@" > "openrc/conf.d/$NAME"
-    execute_template "openrc/init.d.template" "$@" > "openrc/init.d/$NAME"
+    execute_template "openrc/conf.d.template" > "openrc/conf.d/$NAME"
+    execute_template "openrc/init.d.template" > "openrc/init.d/$NAME"
     chmod a+x "openrc/init.d/$NAME"
 }
 
 generate_sysvinit() {
-    NAME="$1"
-
     mkdir -p sysvinit/init.d
     echo "[sysvinit] Generating service file: $NAME"
-    execute_template "sysvinit/service.template" "$@" > "sysvinit/init.d/$NAME"
+    # option 1: use template which makes use of start-stop-daemon, but it is unclear if this is included in yocto
+    # execute_template "sysvinit/service.template" > "sysvinit/init.d/$NAME"
+
+    # option 2: use template which does not use start-stop-daemon
+    execute_template "sysvinit/service-yocto.template" > "sysvinit/init.d/$NAME"
     chmod a+x "sysvinit/init.d/$NAME"
 }
 
-i=0
-while read -r line
+generate_s6_overlay() {
+    shopt -s globstar
+
+    mkdir -p s6-overlay/s6-rc.d
+    echo "[s6-overlay] Generating service files: $NAME"
+
+    rm -rf "s6-overlay/s6-rc.d/$NAME"
+    rm -rf "s6-overlay/s6-rc.d/$NAME-log"
+
+    cp -R s6-overlay/templates/service "s6-overlay/s6-rc.d/$NAME"
+    for f in "s6-overlay/s6-rc.d/$NAME"/**/* ; do
+        if [ -f "$f" ]; then
+            execute_template_inplace "$f"
+        fi
+    done
+
+    cp -R s6-overlay/templates/service-log "s6-overlay/s6-rc.d/$NAME-log"
+    for f in "s6-overlay/s6-rc.d/$NAME-log"/**/* ; do
+        if [ -f "$f" ]; then
+            execute_template_inplace "$f"
+        fi
+    done
+
+    mkdir -p s6-overlay/s6-rc.d/user/contents.d
+    touch "s6-overlay/s6-rc.d/user/contents.d/$NAME-pipeline"
+
+    find "s6-overlay/s6-rc.d" -name run -exec chmod +x {} \;
+}
+
+for f in template-input/*
 do
-    # ignore header
-    if [ "$i" -eq 0 ]; then
-        i=$(( i + 1))
-        continue
+    if [ -f "$f" ]; then
+        # Clear all values
+        NAME=""
+        COMMAND=""
+        COMMAND_ARGS=""
+        COMMAND_USER=""
+        DESCRIPTION=""
+
+        # Source template variables
+        # shellcheck disable=SC1090
+        . "$f"
+
+        # Validate mandatory arguments
+        if [ -n "$NAME" ] && [ -n "$COMMAND" ]; then
+            echo -e "\nGenerating service files using: file = $f - name=$NAME, command=$COMMAND"
+            generate_openrc
+            generate_sysvinit
+            generate_s6_overlay
+        
+        else
+            echo "Missing some mandatory variables. NAME and COMMAND must be set in template input file: $f" >&2
+            exit 1
+        fi
+
     fi
-
-    i_name=$(echo "$line" | cut -d, -f1 )
-    i_cmd=$(echo "$line" | cut -d, -f2 )
-    i_cmd_args=$(echo "$line" | cut -d, -f3 )
-    i_cmd_description=$(echo "$line" | cut -d, -f4 )
-
-    generate_openrc "$i_name" "$i_cmd" "$i_cmd_args" "$i_cmd_description"
-    generate_sysvinit "$i_name" "$i_cmd" "$i_cmd_args" "$i_cmd_description"
-    i=$(( i + 1 ))
-
-done < template_input.csv
+done
 
 popd || exit 1
