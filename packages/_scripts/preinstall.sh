eaae9784e63961f1c84fc5501d3d1b5dac9b49cb
reubenmiller <reuben.d.miller@gmail.com>
2025-03-05 19:54:09 +0100

    fix: add full path to sudoers rule that is being added

--- packages/_scripts/preinstall.sh
+++ packages/_scripts/preinstall.sh
@@ -3,6 +3,6 @@ set -e
 
 # Only add if /etc/sudoers.d exists
 if [ -d /etc/sudoers.d ]; then
-    echo "Adding tedgectl to 100-tedge-tedgectl" >&2
+    echo "Adding tedgectl to sudoers. /etc/sudoers.d/100-tedge-tedgectl" >&2
     echo "tedge    ALL = (ALL) NOPASSWD: /usr/bin/tedgectl" > /etc/sudoers.d/100-tedge-tedgectl
 fi
